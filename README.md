9ee246034c7e14daa0e414794a516ca6bcd0babe
Tom Viner <tom@viner.tv>
2022-03-26 21:01:33 +0000

    add collapse twitter messages tab script

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # userscripts
 Userscripts to amend various websites with tampermonkey
+
+### Install
+https://openuserjs.org/users/tomviner/scripts
+
+### Instructions
+- Install a browser extension to run userscripts, such as [Tampermonkey](http://tampermonkey.net/)
+- Install the [userscripts](https://openuserjs.org/users/tomviner/scripts)
+
+## Spareroom Userscript
+See https://github.com/tomviner/spareroom-userscripts
---
+++ collapse-twitter-messages-tab.js
@@ -0,0 +1,35 @@
+// ==UserScript==
+// @name         Collapse Twitter Messages Tab
+// @namespace    https://github.com/tomviner/userscripts/
+// @version      0.1
+// @description  Twitter Messages Tab is annoying. Auto collapse it.
+// @author       Tom V
+// @license      MIT
+// @homepageURL  https://github.com/tomviner/userscripts
+// @match        *://twitter.com/*
+// @noframes
+// ==/UserScript==
+
+/* jshint ignore:end */
+/* jshint esnext: false */
+/* jshint esversion: 6 */
+
+(function(){
+    'use strict';
+
+    // We rely on Spareroom's jQuery
+    if (typeof(jQuery)  === "undefined"){
+        return;
+    }
+
+    jQuery(function($){
+        let button = document.querySelector('div[aria-label="Collapse"][role="button"]');
+
+        if (!button || button.length === 0){
+            return;
+        }
+
+        button.click();
+    });
+
+})();
